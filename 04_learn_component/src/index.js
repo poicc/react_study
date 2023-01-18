7b5fb8a382a8198ac53bef8e951d1f3f5b6f920e
陈蓉琪 <2560507398@qq.com>
2023-01-18 16:35:35 +0800

    跨组件通信 context API

--- 04_learn_component/src/index.js
+++ 04_learn_component/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './05_React实现slot/App';
+import App from './06_跨组件的通信/04_跨组件通信-多个context';
 
-import './05_React实现slot/style.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
