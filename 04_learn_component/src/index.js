aade6901efebbb6e45032cefa4f163eca9373def
陈蓉琪 <2560507398@qq.com>
2023-01-19 19:57:13 +0800

    高阶组件的使用

--- 04_learn_component/src/index.js
+++ 04_learn_component/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import App from './09_前面知识点补充/02_全局事件传递';
-import App from './10_受控和非受控组件/05_非受控组件的使用';
+import App from './11_高阶组件的使用/01_高阶组件的定义方式';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <App name='aaa'/>
 );
 
