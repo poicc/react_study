bf1808d5812f51cde26e4c86de6f50627be1c254
陈蓉琪 <2560507398@qq.com>
2023-01-19 20:29:27 +0800

    refs的转发 portals\fragments\strictMode使用

--- 04_learn_component/src/index.js
+++ 04_learn_component/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import App from './09_前面知识点补充/02_全局事件传递';
-import App from './11_高阶组件的使用/01_高阶组件的定义方式';
+import App from './12_组件内容补充/04_strictMode的使用';
+import './css/index.css'
 
 
 
