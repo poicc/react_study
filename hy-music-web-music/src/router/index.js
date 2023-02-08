e4aedefd969913dda6932d57a9726f520ea18985
陈蓉琪 <2560507398@qq.com>
2023-02-08 13:35:12 +0800

    整体路由优化 /重定向到发现音乐

--- hy-music-web-music/src/router/index.js
+++ hy-music-web-music/src/router/index.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import { Redirect } from "react-router-dom";
+
 import HYDiscover from "@/pages/discover";
 import HYFriend from "@/pages/friend";
 import HYMine from "@/pages/mine";
@@ -6,6 +9,12 @@ const routes = [
   {
     path: "/",
     exact: true,
+    render: () => (
+      <Redirect to="/discover"/>
+    )
+  },
+  {
+    path: "/discover",
     component: HYDiscover,
   },
   {
