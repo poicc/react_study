619418b779810bd6901920be552206cef5b3f30e
陈蓉琪 <2560507398@qq.com>
2023-02-11 14:54:20 +0800

    新碟上架组件封装

--- hy-music-web-music/src/pages/discover/c-pages/recommend/c-cpns/new-album/index.js
+++ hy-music-web-music/src/pages/discover/c-pages/recommend/c-cpns/new-album/index.js
@@ -3,6 +3,7 @@ import React, { memo, useEffect, useRef } from "react";
 import { getNewAlbumAction } from "../../store/actionCreators";
 
 import { Carousel } from "antd";
+import HYAlbumCover from "@/components/album-cover";
 import HYThemeHeaderRCM from "@/components/theme-header-rcm";
 import { shallowEqual, useDispatch, useSelector } from "react-redux";
 import { AlbumWrapper } from "./style";
@@ -26,21 +27,35 @@ export default memo(function HYNewAlbum() {
     <AlbumWrapper>
       <HYThemeHeaderRCM title="新碟上架" />
       <div className="content">
-        <button className="arrow arrow-left sprite_02" onClick={e => pageRef.current.prev()}></button>
+        <button
+          className="arrow arrow-left sprite_02"
+          onClick={(e) => pageRef.current.prev()}
+        ></button>
         <div className="album">
           <Carousel dots={false} ref={pageRef}>
             {[0, 1].map((item) => {
               return (
                 <div key={item} className="page">
                   {newAlbums.slice(item * 5, (item + 1) * 5).map((iten) => {
-                    return <div key={iten.id}>{iten.name}</div>;
+                    return (
+                      <HYAlbumCover
+                        key={iten.id}
+                        info={iten}
+                        size={100}
+                        width={118}
+                        bgp="-570px"
+                      ></HYAlbumCover>
+                    );
                   })}
                 </div>
               );
             })}
           </Carousel>
         </div>
-        <button className="arrow arrow-right sprite_02" onClick={e => pageRef.current.next()}></button>
+        <button
+          className="arrow arrow-right sprite_02"
+          onClick={(e) => pageRef.current.next()}
+        ></button>
       </div>
     </AlbumWrapper>
   );
