f94062a47299acb1d4db5b73c1b132014ba46a9a
陈蓉琪 <2560507398@qq.com>
2023-03-10 16:01:58 +0800

    歌曲进度切换

--- hy-music-web-music/src/pages/player/app-player-bar/index.js
+++ hy-music-web-music/src/pages/player/app-player-bar/index.js
@@ -1,4 +1,4 @@
-import React, { memo, useEffect, useState } from "react";
+import React, { memo, useEffect, useState, useRef, useCallback } from "react";
 import { useDispatch, useSelector, shallowEqual } from "react-redux";
 
 import { getSizeImage, formatDate, getPlaySong } from "@/utils/format-utils";
@@ -9,6 +9,9 @@ import { getSongDetailAction } from "../store/actionCreators";
 
 export default memo(function HYAppPlayerBar() {
   const [currentTime, setCurrentTime] = useState(0);
+  const [progress, setProgress] = useState(0);
+  const [isChanging, setIsChanging] = useState(false);
+  const [isPlaying, setIsPlaying] = useState(false);
 
   const { currentSong } = useSelector(
     (state) => ({
@@ -19,22 +22,80 @@ export default memo(function HYAppPlayerBar() {
 
   const dispatch = useDispatch();
 
+  const audioRef = useRef();
+
   useEffect(() => {
     dispatch(getSongDetailAction(167876));
   }, [dispatch]);
 
+  useEffect(() => {
+    audioRef.current.src = getPlaySong(currentSong.id);
+    audioRef.current
+      .play()
+      .then((res) => {
+        setIsPlaying(true);
+      })
+      .catch((err) => {
+        setIsPlaying(false);
+      });
+  }, [currentSong]);
+
+  // other handle
   const picUrl = (currentSong.al && currentSong.al.picUrl) || "";
   const singerName = (currentSong.ar && currentSong.ar[0].name) || "未知歌手";
   const duration = currentSong.dt || 0;
   const showDuration = formatDate(duration, "mm:ss");
   const showCurrentTime = formatDate(currentTime, "mm:ss");
 
+  // handle function
+  const playMusic = useCallback(() => {
+    isPlaying ? audioRef.current.pause() : audioRef.current.play();
+    setIsPlaying(!isPlaying);
+  }, [isPlaying]);
+
+  const timeUpdate = (e) => {
+    const currentTime = e.target.currentTime;
+    if (!isChanging) {
+      setCurrentTime(currentTime * 1000);
+      setProgress(((currentTime * 1000) / duration) * 100);
+    }
+  };
+
+  const handleMusicEnded = () => {};
+
+  const sliderChange = useCallback(
+    (value) => {
+      setIsChanging(true);
+      const currentTime = (value / 100) * duration;
+      setCurrentTime(currentTime);
+      setProgress(value);
+    },
+    [duration]
+  );
+
+  const sliderAfterChange = useCallback(
+    (value) => {
+      const currentTime = ((value / 100) * duration) / 1000;
+      audioRef.current.currentTime = currentTime;
+      setCurrentTime(currentTime * 1000);
+      setIsChanging(false);
+
+      if (!isPlaying) {
+        playMusic();
+      }
+    },
+    [duration, isPlaying, playMusic]
+  );
+
   return (
     <PlaybarWrapper className="sprite_player">
       <div className="content wrap-v2">
         <Control>
           <button className="sprite_player prev"></button>
-          <button className="sprite_player play"></button>
+          <button
+            className="sprite_player play"
+            onClick={(e) => playMusic()}
+          ></button>
           <button className="sprite_player next"></button>
         </Control>
         <PlayInfo>
@@ -51,9 +112,14 @@ export default memo(function HYAppPlayerBar() {
               </a>
             </div>
             <div className="progress">
-              <Slider defaultValue={30} />
+              <Slider
+                defaultValue={30}
+                value={progress}
+                onChange={sliderChange}
+                onAfterChange={sliderAfterChange}
+              />
               <div className="time">
-                <span className="now-time">02:30</span>
+                <span className="now-time">{showCurrentTime}</span>
                 <span className="divider">/</span>
                 <span className="duration">{showDuration}</span>
               </div>
@@ -72,6 +138,11 @@ export default memo(function HYAppPlayerBar() {
           </div>
         </Operator>
       </div>
+      <audio
+        ref={audioRef}
+        onTimeUpdate={(e) => timeUpdate(e)}
+        onEnded={(e) => handleMusicEnded()}
+      />
     </PlaybarWrapper>
   );
 });
