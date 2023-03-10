cfc1dbbc7d59b2e0d00ea8047d75c86bf1bfe440
陈蓉琪 <2560507398@qq.com>
2023-03-10 17:45:47 +0800

    路由懒加载

--- hy-music-web-music/src/router/index.js
+++ hy-music-web-music/src/router/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import { Redirect } from "react-router-dom";
 
-import HYDiscover from "@/pages/discover";
-import HYRecommend from "../pages/discover/c-pages/recommend";
-import HYRanking from "../pages/discover/c-pages/ranking";
-import HYSongs from "../pages/discover/c-pages/songs";
-import HYDjradio from "../pages/discover/c-pages/djradio";
-import HYArtist from "../pages/discover/c-pages/artist";
-import HYAlbum from "../pages/discover/c-pages/album";
-import HYPlayer from "../pages/player";
+const HYDiscover = React.lazy(() => import("@/pages/discover"));
+const HYRecommend = React.lazy(_ => import("../pages/discover/c-pages/recommend"));
+const HYRanking = React.lazy(_ => import("../pages/discover/c-pages/ranking"));
+const HYSongs = React.lazy(_ => import("../pages/discover/c-pages/songs"));
+const HYDjradio = React.lazy(_ => import("../pages/discover/c-pages/djradio"));
+const HYArtist = React.lazy(_ => import("../pages/discover/c-pages/artist"));
+const HYAlbum = React.lazy(_ => import("../pages/discover/c-pages/album"));
+const HYPlayer = React.lazy(_ => import("../pages/player"));
 
-import HYFriend from "@/pages/friend";
-import HYMine from "@/pages/mine";
+const HYFriend = React.lazy(_ => import("../pages/friend"));
+const HYMine = React.lazy(_ => import("../pages/mine"));
+
+// import HYDiscover from "@/pages/discover";
+// import HYRecommend from "../pages/discover/c-pages/recommend";
+// import HYRanking from "../pages/discover/c-pages/ranking";
+// import HYSongs from "../pages/discover/c-pages/songs";
+// import HYDjradio from "../pages/discover/c-pages/djradio";
+// import HYArtist from "../pages/discover/c-pages/artist";
+// import HYAlbum from "../pages/discover/c-pages/album";
+// import HYPlayer from "../pages/player";
+
+// import HYMine from "@/pages/mine";
+// import HYFriend from "@/pages/friend";
 
 const routes = [
   {
